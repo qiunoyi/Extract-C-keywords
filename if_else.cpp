046055e03c3fc4a284516a3fd8647a42243edce6
qiunoyi <18359778223@163.com>
2021-09-19 10:15:56 +0800

    if_else

--- if_else.cpp
+++ if_else.cpp
@@ -1,9 +1,50 @@
-#include<iostream>
-#include<stack>
-#include<fstream>
-#include<algorithm>
+#include <iostream>
+#include <stack>
+#include <fstream>
+#include <algorithm>
 using namespace std;
+int if_else_num = 0;
+int if_else_if_else_num = 0;
+//栈设计
+stack<char> sta;
+void sta_input(char c)
+{
+    switch (c)
+    {
+    case '}':
+        while (sta.top() != '{')
+        {
+            sta.pop();
+        }
+        sta.pop();
+        break;
+    case '{':
+        sta.push('{');
+        break;
+    case '1':
+        sta.push('1');
+        break;
+    case '2':
+        sta.push('2');
+        break;
+    case '3':
+        int count = 0;
+        while (sta.top() != '1')
+        {
+            ++count;
+            sta.pop();
+        }
+        sta.pop();
+        if (count > 0)
+            ++if_else_if_else_num;
+        else
+            ++if_else_num;
+        break;
+    default:
+        cout << "字符输入错误" << endl;
+        break;
+    }
+}
 int main()
 {
-    
 }
